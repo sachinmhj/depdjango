d3097174f4dd980ee0b6fe229b73cc17932e4fe8
sachin maharjan <scnmrsn9818@gmail.com>
2022-06-27 12:35:42 +0545

    6

--- main/settings.py
+++ main/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%g2q7)afcuo(xr+zxjset0m3ep3o=9@to*zl9p&qv%r^9kfvrz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
